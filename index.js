6c4d9c6a14a071aa7943a52c6283cfc7d44b9f17
gilbertkawun <gilbertkawun@gmail.com>
2022-06-06 19:16:01 +0700

    adding sprites animation for taking hit and death for both players

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ ce.fillRect(0, 0, canvas.width, canvas.height);
 const gravity = 0.7;
 
 const background = new Sprites({
-    position: {
-        x: 0,
-        y: 0
-    },
-    imgSrc: './sprites/PNG/cyberpunk-street-resize.png'
-})
+  position: {
+    x: 0,
+    y: 0,
+  },
+  imgSrc: "./sprites/PNG/cyberpunk-street-resize.png",
+});
 
 const players = new Fighters({
-  position: {  
+  position: {
     x: 0,
     y: 0,
   },
@@ -30,34 +30,42 @@ const players = new Fighters({
     x: 0,
     y: 0,
   },
-  imgSrc: './sprites/player-1/Idle.png',
+  imgSrc: "./sprites/player-1/Idle.png",
   framesMax: 4,
   scale: 2.5,
   offset: {
     x: 180,
-    y: 180
+    y: 180,
   },
   sprites: {
     idle: {
-      imgSrc: './sprites/player-1/Idle.png',
-      framesMax: 4
+      imgSrc: "./sprites/player-1/Idle.png",
+      framesMax: 4,
     },
     run: {
-      imgSrc: './sprites/player-1/Run.png',
-      framesMax: 8
+      imgSrc: "./sprites/player-1/Run.png",
+      framesMax: 8,
     },
     jump: {
-      imgSrc: './sprites/player-1/Jump.png',
-      framesMax: 2
+      imgSrc: "./sprites/player-1/Jump.png",
+      framesMax: 2,
     },
     fall: {
-      imgSrc: './sprites/player-1/Fall.png',
-      framesMax: 2
+      imgSrc: "./sprites/player-1/Fall.png",
+      framesMax: 2,
     },
     attack1: {
-      imgSrc: './sprites/player-1/Attack1.png',
-      framesMax: 4
-    }
+      imgSrc: "./sprites/player-1/Attack1.png",
+      framesMax: 4,
+    },
+    takeHit: {
+      imgSrc: "./sprites/player-1/Take hit.png",
+      framesMax: 3,
+    },
+    death: {
+      imgSrc: "./sprites/player-1/Death.png",
+      framesMax: 7,
+    },
   },
   attack: {
     offset: {
@@ -65,10 +73,10 @@ const players = new Fighters({
       y: 50,
     },
     width: 150,
-    height: 50
-  }
+    height: 50,
+  },
 });
- 
+
 const enemies = new Fighters({
   position: {
     x: 576,
@@ -83,37 +91,42 @@ const enemies = new Fighters({
     y: 0,
   },
   color: "blue",
-  imgSrc: './sprites/player-2/Idle.png',
+  imgSrc: "./sprites/player-2/Idle.png",
   framesMax: 11,
   scale: 2.5,
   offset: {
     x: 200,
-    y: 150
+    y: 150,
   },
   sprites: {
     idle: {
-      imgSrc: './sprites/player-2/Idle.png',
-      framesMax: 11
+      imgSrc: "./sprites/player-2/Idle.png",
+      framesMax: 11,
     },
     run: {
-      imgSrc: './sprites/player-2/Run.png',
-      framesMax: 8
+      imgSrc: "./sprites/player-2/Run.png",
+      framesMax: 8,
     },
     jump: {
-      imgSrc: './sprites/player-2/Jump.png',
-      framesMax: 3
+      imgSrc: "./sprites/player-2/Jump.png",
+      framesMax: 3,
     },
     fall: {
-      imgSrc: './sprites/player-2/Fall.png',
-      framesMax: 3
+      imgSrc: "./sprites/player-2/Fall.png",
+      framesMax: 3,
     },
     attack1: {
-      imgSrc: './sprites/player-2/Attack1.png',
-      framesMax: 7
+      imgSrc: "./sprites/player-2/Attack1.png",
+      framesMax: 7,
     },
     takeHit: {
-      imgSrc: './sprites/player-2/Take Hit.png'
-    }
+      imgSrc: "./sprites/player-2/Take Hit.png",
+      framesMax: 4,
+    },
+    death: {
+      imgSrc: "./sprites/player-2/Death.png",
+      framesMax: 11,
+    },
   },
   attack: {
     offset: {
@@ -121,8 +134,8 @@ const enemies = new Fighters({
       y: 50,
     },
     width: 150,
-    height: 50
-  }
+    height: 50,
+  },
 });
 
 const keys = {
@@ -146,14 +159,13 @@ const keys = {
   },
 };
 
-
 timedownTimer();
 
 function animate() {
   window.requestAnimationFrame(animate);
   ce.fillStyle = "black";
   ce.fillRect(0, 0, canvas.width, canvas.height);
-  background.update()
+  background.update();
   players.update();
   enemies.update();
 
@@ -162,35 +174,35 @@ function animate() {
 
   if (keys.a.pressed && players.lastKey === "a") {
     players.velocity.x = -5;
-    players.animationSwitch('run')
+    players.animationSwitch("run");
   } else if (keys.d.pressed && players.lastKey === "d") {
     players.velocity.x = 5;
-    players.animationSwitch('run')
+    players.animationSwitch("run");
   } else {
-    players.animationSwitch('idle')
+    players.animationSwitch("idle");
   }
 
   if (players.velocity.y < 0) {
-    players.animationSwitch('jump')
+    players.animationSwitch("jump");
   } else if (players.velocity.y > 0) {
-    players.animationSwitch('fall')
+    players.animationSwitch("fall");
   }
 
   //enemy
   if (keys.ArrowLeft.pressed && enemies.lastKey === "ArrowLeft") {
     enemies.velocity.x = -5;
-    enemies.animationSwitch('run')
+    enemies.animationSwitch("run");
   } else if (keys.ArrowRight.pressed && enemies.lastKey === "ArrowRight") {
     enemies.velocity.x = 5;
-    enemies.animationSwitch('run')
+    enemies.animationSwitch("run");
   } else {
-    enemies.animationSwitch('idle')
+    enemies.animationSwitch("idle");
   }
 
   if (enemies.velocity.y < 0) {
-    enemies.animationSwitch('jump')
+    enemies.animationSwitch("jump");
   } else if (enemies.velocity.y > 0) {
-    enemies.animationSwitch('fall')
+    enemies.animationSwitch("fall");
   }
 
   // collision
@@ -199,16 +211,18 @@ function animate() {
       obj1: players,
       obj2: enemies,
     }) &&
-    players.onAttack && players.framesCurrent === 2
+    players.onAttack &&
+    players.framesCurrent === 2
   ) {
+    enemies.takeHit();
     players.onAttack = false;
-    enemies.health -= 20;
+    // enemies.health -= 20;
     document.querySelector("#enemyHealth").style.width = enemies.health + "%";
   }
 
   // miss attack
   if (players.onAttack && players.framesCurrent === 2) {
-    players.onAttack = false
+    players.onAttack = false;
   }
 
   if (
@@ -216,17 +230,19 @@ function animate() {
       obj1: enemies,
       obj2: players,
     }) &&
-    enemies.onAttack && enemies.framesCurrent === 1
+    enemies.onAttack &&
+    enemies.framesCurrent === 1
   ) {
+    players.takeHit();
     enemies.onAttack = false;
-    players.health -= 20;
+    // players.health -= 20;
     document.querySelector("#playerHealth").style.width = players.health + "%";
   }
 
-    // miss attack
-    if (enemies.onAttack && enemies.framesCurrent === 1) {
-      enemies.onAttack = false
-    }
+  // miss attack
+  if (enemies.onAttack && enemies.framesCurrent === 1) {
+    enemies.onAttack = false;
+  }
 
   // result
   if (enemies.health <= 0 || players.health <= 0) {
@@ -237,36 +253,42 @@ function animate() {
 animate();
 
 window.addEventListener("keydown", (click) => {
-  switch (click.key) {
-    case "d":
-      keys.d.pressed = true;
-      players.lastKey = "d";
-      break;
-    case "a":
-      keys.a.pressed = true;
-      players.lastKey = "a";
-      break;
-    case "w":
-      players.velocity.y = -18;
-      break;
-    case " ":
-      players.attacking();
-      break;
+  if (!players.dead) {
+    switch (click.key) {
+      case "d":
+        keys.d.pressed = true;
+        players.lastKey = "d";
+        break;
+      case "a":
+        keys.a.pressed = true;
+        players.lastKey = "a";
+        break;
+      case "w":
+        players.velocity.y = -18;
+        break;
+      case " ":
+        players.attacking();
+        break;
+    }
+  }
 
-    case "ArrowRight":
-      keys.ArrowRight.pressed = true;
-      enemies.lastKey = "ArrowRight";
-      break;
-    case "ArrowLeft":
-      keys.ArrowLeft.pressed = true;
-      enemies.lastKey = "ArrowLeft";
-      break;
-    case "ArrowUp":
-      enemies.velocity.y = -18;
-      break;
-    case "ArrowDown":
-      enemies.attacking();
-      break;
+  if (!enemies.dead) {
+    switch (click.key) {
+      case "ArrowRight":
+        keys.ArrowRight.pressed = true;
+        enemies.lastKey = "ArrowRight";
+        break;
+      case "ArrowLeft":
+        keys.ArrowLeft.pressed = true;
+        enemies.lastKey = "ArrowLeft";
+        break;
+      case "ArrowUp":
+        enemies.velocity.y = -18;
+        break;
+      case "ArrowDown":
+        enemies.attacking();
+        break;
+    }
   }
 });
 
